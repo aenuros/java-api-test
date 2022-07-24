032610b0e1b823fd337659192b26eda3e60cd98a
Karen Burgos <karen@burgos.us>
2022-07-24 19:46:47 -0400

    rename

---
+++ src/main/java/com/example/RestService/sqlDataBooks.java
@@ -0,0 +1,91 @@
+package com.example.RestService;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class sqlDataBooks {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/books")
+	public String allBooks(@RequestParam(value = "query", defaultValue = "SELECT * FROM book") String query) {
+		return new sqlData(query).returnData;
+	}
+	
+	@GetMapping("/books/bygenre")
+	public String booksByGenre(@RequestParam(value = "genre") String genre){
+		return new sqlData("SELECT * FROM book WHERE genre = \"" + genre + "\"").returnData;
+	}
+	
+	
+	@GetMapping("/books/bymostsold")
+	public String booksByGenre(){
+		return new sqlData("SELECT * FROM book ORDER BY copies_sold LIMIT 10").returnData;
+	}
+	
+	// Must be able to retrieve a list of ratings and comments, sorted by highest rating
+	@GetMapping("/books/rating")
+	public String booksByRating(){
+		return new sqlDataGetBookByRating("SELECT bookshop.rating.stars,\n"
+				+ "bookshop.rating.comment,\n"
+				+ "bookshop.book.Name,\n"
+				+ "bookshop.user.username,\n"
+				+ "bookshop.rating.datetime\n"
+				+ "FROM bookshop.rating\n"
+				+ "JOIN bookshop.book ON bookshop.rating.book_id = bookshop.book.book_id\n"
+				+ "JOIN bookshop.user ON bookshop.rating.user_id = bookshop.user.user_id\n"
+				+ "ORDER BY bookshop.rating.stars DESC;\n"
+				+ "\n"
+				+ "").returnData;
+	}
+	
+	@GetMapping("/books/avgrating")
+	public String booksByAverageRating(@RequestParam(value = "id") String id) {
+		return new sqlDataGetBookByAverageRating("SELECT bookshop.rating.stars,\n"
+				+ "bookshop.rating.comment,\n"
+				+ "bookshop.book.Name, \n"
+				+ "bookshop.user.username,\n"
+				+ "bookshop.rating.datetime\n"
+				+ "FROM bookshop.rating\n"
+				+ "JOIN bookshop.book ON bookshop.rating.book_id = bookshop.book.book_id\n"
+				+ "JOIN bookshop.user ON bookshop.rating.user_id = bookshop.user.user_id\n"
+				+ "WHERE bookshop.rating.book_id = " + id + "\n"
+				+ "ORDER BY bookshop.rating.stars DESC;").returnData;
+	}
+
+//	@RequestMapping(value = "/books/newrating")
+//	  public String newRating() {
+//		  return new sqlDataInsertRating("INSERT INTO bookshop.rating (stars, book_id, user_id, comment) \n"
+//		  		+ "VALUES ('1', '10', '4', 'I am not a fan');").returnData;
+//	  }
+	
+	
+//	@RequestMapping(value = "/books/newrating")
+//	  public String newRating(@RequestParam(value = "query", defaultValue = "SELECT * FROM book") String query) {
+//		  return new sqlDataInsertRating("INSERT INTO bookshop.rating (stars, book_id, user_id, comment) \n"
+//		  		+ "VALUES ('1', '10', '4', 'I am not a fan');").returnData;
+//	  }
+	
+	@RequestMapping("/books/newrating")
+	public String newRating(@RequestParam(value = "stars") int stars,
+			@RequestParam(value="book_id") int book_id,
+			@RequestParam(value="user_id") int user_id,
+			@RequestParam(value="comment") String comment
+			){
+//		return new sqlData("SELECT * FROM book WHERE genre = \"" + genre + "\"").returnData;
+		return new sqlDataInsertRating("INSERT INTO bookshop.rating (stars, book_id, user_id, comment) \n"
+		  		+ "VALUES ('" + 1 + "', '" + 10 + "', '" + 4 + "', '" + comment + "');").returnData;
+	}
+	
+	
+
+	
+}
+
